721e3047c221ee7962180f7e870e317c6500aff9
Damo Tamas <tdamo@pentalog.fr>
2014-04-15 17:15:36 +0300

    added agent list toggling to view 3 last visits

--- app/src/core/datamart/users/ViewOneController.js
+++ app/src/core/datamart/users/ViewOneController.js
@@ -17,7 +17,7 @@
       var userEndpoint = Restangular.one('datamarts', 8).one('users', $routeParams.userId);
       userEndpoint.get().then(function (user) {
         if (user.account_creation_date) {
-          user.account_creation_date = new Date(user.account_creation_date);
+          //user.account_creation_date = new Date(parseInt(user.account_creation_date));
         }
         $scope.user = user;
 
@@ -26,6 +26,24 @@
           $scope.agents = agents;
         });
       });
+
+      // click handler for Agents
+      $scope.expandAgent = function($event){
+        var agent = this.agent;
+        if (agent.expanded) {
+          // collapse
+          agent.expanded = false;
+          $event.currentTarget.classList.remove('active');
+        } else {
+          // expand, fetch last visits
+          agent.expanded = true;
+          $event.currentTarget.classList.add('active');
+          Restangular.one('datamarts', 8).one('agents', agent.id).one('visits').one('last').get().then(function (visits) {
+            agent.visits = visits;
+          });
+        }
+        
+      }
     }
   ]);
 
