4dd47b5c9f15a82baa85fb135991b14a49e6cf8d
mgrenonville <mgrenonville@mediarithmics.com>
2014-05-02 11:42:58 +0200

    May we add a cache ?

--- app/src/core/creatives/CreativeDirective.js
+++ app/src/core/creatives/CreativeDirective.js
@@ -6,13 +6,18 @@
   module.directive('creativeThumbnail', [
     "Restangular",
     function (Restangular) {
+      var CreativeRestangular = Restangular.withConfig(
+        function(RestangularConfigurer) {
+//          RestangularConfigurer.setDefaultHttpFields({cache: true});
+      });
+
       return {
         restrict: 'E',
         scope: {
           creativeId: '=creativeId'
         },
         controller:function ($scope) {
-          $scope.creative = Restangular.one("creatives", $scope.creativeId).get().$object;
+          $scope.creative = CreativeRestangular.one("creatives", $scope.creativeId).get().$object;
 
         },
         templateUrl:"src/core/creatives/creative-thumbnail.html"
