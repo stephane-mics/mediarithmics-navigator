7f3d874780a434e0c13181666fc6364f104956e4
mgrenonville <mgrenonville@mediarithmics.com>
2014-05-20 17:40:20 +0200

    Deal with creative thumbnails and rendererProperties

--- app/src/core/creatives/CreativeDirective.js
+++ app/src/core/creatives/CreativeDirective.js
@@ -2,11 +2,9 @@
   'use strict';
 
 
-  var module = angular.module('core/creatives');
-  
-  module.directive('creativeThumbnail', [
-    "Restangular",
-    function (Restangular) {
+  var module = angular.module('core/creative');
+  module.directive('creativeThumbnail', ["Restangular", 'core/configuration',
+    function (Restangular, configuration) {
 //      var CreativeRestangular = Restangular.withConfig(
 //        function (RestangularConfigurer) {
 //          RestangularConfigurer.setDefaultHttpFields({cache: true});
@@ -18,15 +16,26 @@
             if (!value) {
               return;
             }
-            attrs.$set("src", "http://www.flaticon.com/png/256/12380.png");
+            Restangular.one("creatives", value).one("thumbnail").get().then(
+              function (creative) {
+                if (creative.asset_path) {
+                  attrs.$set("src", configuration.ASSETS_URL + creative.asset_path);
+                } else if (creative.icon_id === "flash") {
+                  attrs.$set("src", "images/Adobe-swf_icon.png");
+                } else if (creative.icon_id === "image") {
+                  attrs.$set("src", "images/Unknown_file.png");
+                } else if (creative.url) {
+                  attrs.$set("src", creative.url);
+                }
 
-            // on IE, if "ng:src" directive declaration is used and "src" attribute doesn't exist
-            // then calling element.setAttribute('src', 'foo') doesn't do anything, so we need
-            // to set the property as well to achieve the desired effect.
-            // we use attr[attrName] value since $set can sanitize the url.
-//            if (msie) {
-//              element.prop("src", attrs["src"]);
-//            }
+              }
+            ).catch(
+              function (error) {
+                attrs.$set("src", "images/Unknown_file.png");
+              }
+            );
+
+
           });
         }
 
@@ -49,13 +58,29 @@
         compile: function (element, attr, linker) {
           return function ($scope, $element, $attr) {
             var asString = $attr.fetchCreative;
-            var match = asString.match(/^\s*(.+)\s+as\s+(.*?)\s*$/);
+            //ad.creative_id as creative with rendererProperties
+            var match = asString.match(/^\s*(.+)\s+as\s+(.*?)\s*(with\s*(.*))?$/);
             var creativeIdExpr = match[1];
             var exposedVar = match[2];
+            var withRendererProperties = match[4] === "rendererProperties";
+
             var parent = $element.parent();
             $scope.$watch(creativeIdExpr, function (newValue, oldValue, scope) {
               if (newValue !== undefined) {
-                $scope[exposedVar] = Restangular.one("creatives", newValue).get().$object;
+                var creative = Restangular.one("creatives", newValue);
+                $scope[exposedVar] = creative.get().$object;
+                if (withRendererProperties) {
+                  Restangular.one("display_ads", newValue).all("renderer_properties").getList().then(
+                    function (properties) {
+                      var result = {};
+                      for (var i = 0; i < properties.length; i++) {
+                        var p = properties[i];
+                        result[p.technical_name] = {value: p.value, property_type: p.property_type};
+                      }
+                      $scope[exposedVar + "Properties"] = result;
+                    }
+                  );
+                }
               }
               linker($scope, function (clone) {
                 // clone the transcluded element, passing in the new scope.
@@ -74,6 +99,19 @@
       };
     }
   ]);
+
+  module.filter('rendererProperty', function () {
+    return function (input) {
+      if (input === undefined) {
+        return;
+      }
+      if (input.property_type === 'URL') {
+        return input.value.url;
+      }
+
+      return "";
+    };
+  });
 
 
 }());
