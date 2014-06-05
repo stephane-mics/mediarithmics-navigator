d493299cf1918c585d27eb3493fd9e8cb865b2c8
David Duponchel <dduponchel@mediarithmics.com>
2014-06-05 15:33:07 +0200

    add a deletion button on the campaign list

--- app/src/core/campaigns/ListController.js
+++ app/src/core/campaigns/ListController.js
@@ -17,8 +17,8 @@
    * Campaign list controller
    */
   module.controller('core/campaigns/ListController', [
-    '$scope', '$location', '$log', 'Restangular', 'd3', 'moment', 'core/campaigns/DisplayCampaignService', 'core/common/auth/Session','CampaignAnalyticsReportService', 'core/campaigns/CampaignPluginService',
-    function ($scope, $location, $log, Restangular, d3, moment, DisplayCampaignService, Session, CampaignAnalyticsReportService, CampaignPluginService) {
+    '$scope', '$location', '$log', 'Restangular', 'd3', 'moment', 'core/campaigns/DisplayCampaignService', 'core/common/auth/Session','CampaignAnalyticsReportService', 'core/campaigns/CampaignPluginService', '$modal',
+    function ($scope, $location, $log, Restangular, d3, moment, DisplayCampaignService, Session, CampaignAnalyticsReportService, CampaignPluginService, $modal) {
 
       $scope.currentPageDisplayCampaign = 1;
       $scope.currentPageEmailCampaign = 1;
@@ -65,6 +65,22 @@
           var location = template.editor.edit_path.replace(/{id}/g, campaign.id);
           $location.path(location);
         });
+        return false;
+      };
+      $scope.deleteCampaign = function (campaign, $event) {
+        if ($event) {
+          $event.preventDefault();
+          $event.stopPropagation();
+        }
+        var newScope = $scope.$new(true);
+        newScope.campaign = campaign;
+        $modal.open({
+          templateUrl: 'src/core/campaigns/delete.html',
+          scope : newScope,
+          backdrop : 'static',
+          controller: 'core/campaigns/DeleteController'
+        });
+
         return false;
       };
     }
