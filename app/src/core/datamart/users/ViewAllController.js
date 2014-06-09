c3f95c4a9101dffd14f6b891dfce33ef9e2da970
stephane <stephane@mediarithmics.com>
2014-06-09 20:28:42 +0200

    test on datamart scope limitation

--- app/src/core/datamart/users/ViewAllController.js
+++ app/src/core/datamart/users/ViewAllController.js
@@ -5,13 +5,15 @@
   var module = angular.module('core/datamart');
 
   module.controller('core/datamart/users/ViewAllController', [
-    '$scope', 'Restangular', 'core/datamart/common/Common',
-    function($scope, Restangular, Common) {
+    '$scope', 'Restangular', 'core/datamart/common/Common', 'core/common/auth/Session',
+    function($scope, Restangular, Common, Session) {
 
       $scope.baseUrl = '#' + Common.locations.current.href;
 
       // TODO: get organisationId from session, get appropriate datamartId
-      $scope.datamartId = 8;
+      var workspace = Session.getCurrentWorkspace();
+      if (workspace.organisation_id == "501") $scope.datamartId = 8;
+      else $scope.datamartId = 0;
 
       $scope.refreshUsers = function (offset, limit) {
         Restangular.one('datamarts', $scope.datamartId).all('users/search/').getList({ terms: $scope.searchTerms, offset: offset, limit: limit}).then(function (result) {
