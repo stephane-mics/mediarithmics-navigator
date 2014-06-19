31a2a1a523e69e934239f543e70ed7ebffcb356a
David Duponchel <dduponchel@mediarithmics.com>
2014-06-19 12:15:47 +0200

    Library : add keywords lists

---
+++ app/src/core/keywords/DeleteController.js
@@ -0,0 +1,27 @@
+(function(){
+  'use strict';
+
+  var module = angular.module('core/keywords');
+
+  module.controller('core/keywords/DeleteController', [
+    '$scope', '$modalInstance', '$location', '$route',
+    function($scope, $modalInstance, $location, $route) {
+
+      $scope.done = function() {
+        $scope.keywordsList.remove().then(function (){
+          $modalInstance.close();
+          $location.path("/library/keywordslists");
+          $route.reload();
+        });
+      };
+
+      $scope.cancel = function() {
+        $modalInstance.close();
+      };
+
+    }
+  ]);
+})();
+
+
+
