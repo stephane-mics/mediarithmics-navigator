8858693581c2ab71ef600bebf8c50028681578f1
Thomas Tosoni <ttosoni@mediarithmics.com>
2015-05-28 10:55:05 +0200

    Move Datamart Routes To Where They Belong

---
+++ app/src/core/datamart/router.js
@@ -0,0 +1,38 @@
+define(['./module'], function (module) {
+  'use strict';
+
+  module.config([
+    "$stateProvider",
+    function ($stateProvider) {
+      $stateProvider
+        .state('datamart/overview', {
+          url: '/datamart/overview',
+          templateUrl: 'src/core/datamart/index.html'
+        })
+        .state('datamart/items', {
+          url: '/datamart/items',
+          templateUrl: 'src/core/datamart/items/view.all.html'
+        })
+        .state('datamart/items/:catalogId/:itemId', {
+          url: '/datamart/items/:catalogId/:itemId',
+          templateUrl: 'src/core/datamart/items/view.one.html'
+        })
+        .state('datamart/categories/:catalogId', {
+          url: '/datamart/categories/:catalogId',
+          templateUrl: 'src/core/datamart/categories/browse.html'
+        })
+        .state('datamart/categories/:catalogId/:categoryId', {
+          url: '/datamart/categories/:catalogId/:categoryId',
+          templateUrl: 'src/core/datamart/categories/browse.html'
+        })
+        .state('datamart/users', {
+          url: '/datamart/users',
+          templateUrl: 'src/core/datamart/users/view.all.html'
+        })
+        .state('datamart/users/:userId', {
+          url: '/datamart/users/:userId',
+          templateUrl: 'src/core/datamart/users/view.one.html'
+        });
+    }
+  ]);
+});
