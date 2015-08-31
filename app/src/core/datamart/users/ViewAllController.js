4e26039583d0a52747b67e7ef334a3fb104e0586
Nicolas Dussart <ndussart@mediarithmics.com>
2015-08-31 17:26:26 +0200

    NC-16 fix search account input

--- app/src/core/datamart/users/ViewAllController.js
+++ app/src/core/datamart/users/ViewAllController.js
@@ -12,17 +12,14 @@ define(['./module'], function (module) {
       $scope.datamartId = Session.getCurrentDatamartId();
       $scope.myTimelineHref = "#/"+Session.getCurrentDatamartId() +"/datamart/users/upid/my_user_point_id?live=true";
 
-      $scope.refreshUsers = function (offset, limit) {
-        Restangular.one('datamarts', $scope.datamartId).all('user_profiles/search/').getList({ terms: $scope.searchTerms, offset: offset, limit: limit}).then(function (result) {
+      $scope.refreshUsers = function (searchTerms,offset, limit) {
+        Restangular.one('datamarts', $scope.datamartId).all('user_profiles/search/').getList({ terms: searchTerms, offset: offset, limit: limit}).then(function (result) {
           $scope.users = result;
         });
       };
 
       // attach watcher
-      $scope.refreshUsers(0, 10);
-      $scope.$watch('searchTerms', function(){
-        $scope.refreshUsers(0, 10);
-      });
+      $scope.refreshUsers("", 0, 20);
 
     }
   ]);
