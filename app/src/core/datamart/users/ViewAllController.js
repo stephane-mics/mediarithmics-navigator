a046b2816fed0237e73eb3a333d33ad0ef5dadf5
Nicolas Dussart <ndussart@mediarithmics.com>
2015-09-01 18:17:30 +0200

    fix pagination again

--- app/src/core/datamart/users/ViewAllController.js
+++ app/src/core/datamart/users/ViewAllController.js
@@ -10,16 +10,19 @@ define(['./module'], function (module) {
       $scope.baseUrl = '#' + Common.locations.current.href;
 
       $scope.datamartId = Session.getCurrentDatamartId();
+
       $scope.myTimelineHref = "#/"+$stateParams.organisation_id +"/datamart/users/upid/my_user_point_id/live/true";
 
-      $scope.refreshUsers = function (searchTerms,offset, limit) {
-        Restangular.one('datamarts', $scope.datamartId).all('user_profiles/search/').getList({ terms: searchTerms, offset: offset, limit: limit}).then(function (result) {
+      $scope.searchTerms = {term:""};
+
+      $scope.refreshUsers = function (offset, limit) {
+        Restangular.one('datamarts', $scope.datamartId).all('user_profiles/search/').getList({ terms: $scope.searchTerms.term, offset: offset, limit: limit}).then(function (result) {
           $scope.users = result;
         });
       };
 
       // attach watcher
-      $scope.refreshUsers("", 0, 20);
+      $scope.refreshUsers(0, 10);
 
     }
   ]);
