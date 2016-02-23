c5e543baad9ed52e97626bac11e5041a8bd580c1
Thomas Tosoni <ttosoni@mediarithmics.com>
2016-02-23 14:01:50 +0100

    Fix route

--- app/src/core/datamart/categories/BrowseController.js
+++ app/src/core/datamart/categories/BrowseController.js
@@ -37,7 +37,7 @@ define(['./module'], function (module) {
           });
         };
 
-        Restangular.one('datamarts', $scope.datamartId).one('deprecated/catalogs', $stateParams.catalogId).one('categories', $stateParams.categoryId).get().then(function (result){
+        Restangular.one('datamarts', $scope.datamartId).one('catalogs', $stateParams.catalogId).one('categories', $stateParams.categoryId).get().then(function (result){
           $scope.currentCategory = result;
           $scope.refreshCategories();
           $scope.refreshDatasheets();
