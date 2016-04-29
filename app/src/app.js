08b685e02a668c63c60ceb923cde5429cccd0e1f
mgrenonville <mgrenonville@mediarithmics.com>
2016-04-29 17:22:03 +0200

    Fix first redirection according to publicUrl
    If state.publicUrl, don't redirect to /login

--- app/src/app.js
+++ app/src/app.js
@@ -4,8 +4,8 @@ define(['app-setup', 'angularAMD', 'jquery'],
 
     jQuery("#mics_loading").remove();
 
-    app.run(['$rootScope', '$location', '$log', 'core/common/auth/AuthenticationService', 'core/common/auth/Session', "lodash", "core/login/constants", "core/common/ErrorReporting","$state","$stateParams",
-      function ($rootScope, $location, $log, AuthenticationService, Session, _, LoginConstants, ErrorReporting, $state, $stateParams ) {
+    app.run(['$rootScope', '$location', '$log', 'core/common/auth/AuthenticationService', 'core/common/auth/Session', "lodash", "core/login/constants", "core/common/ErrorReporting","$state","$stateParams", "$urlRouter",
+      function ($rootScope, $location, $log, AuthenticationService, Session, _, LoginConstants, ErrorReporting, $state, $stateParams, $urlRouter ) {
         var defaults = _.partialRight(_.assign, function (a, b) {
           return typeof a === 'undefined' ? b : a;
         });
@@ -40,14 +40,29 @@ define(['app-setup', 'angularAMD', 'jquery'],
             $location.path('/init-session');
           }
         } else if (AuthenticationService.hasRefreshToken()) {
+          $log.debug("has refresh token -> remember-me");
           // Keep the current path in memory
           AuthenticationService.pushPendingPath($location.url());
           // Redirect to the remember-me page
           $location.path('/remember-me');
         } else {
-          AuthenticationService.pushPendingPath($location.url());
-          // Redirect to login
-          $location.path('/login');
+          var parsedStateFromUrl = _($state.get()).find(function(s) {
+            // http://stackoverflow.com/questions/29892353/angular-ui-router-resolve-state-from-url/30926025#30926025
+
+            if(s.$$state && s.$$state().url) {
+              var urlAndQuery = $location.url().split("?");
+              return !!s.$$state().url.exec(urlAndQuery[0], urlAndQuery[1]);
+            } else {
+              return false;
+            }
+          });
+          $log.debug("parsed state from url : ", parsedStateFromUrl);
+          if (!parsedStateFromUrl || !parsedStateFromUrl.publicUrl) {
+            $log.debug("not a public url, go to login");
+            AuthenticationService.pushPendingPath($location.url());
+            // Redirect to login
+            $location.path('/login');
+          }
         }
 
 
@@ -80,30 +95,34 @@ define(['app-setup', 'angularAMD', 'jquery'],
         $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
           $log.debug("$stateChangeStart ", fromState.url, " : ", toState.url);
           $log.debug("$stateChangeStart ", fromParams, " : ", toParams);
-          if (!AuthenticationService.hasRefreshToken()) {
-            if(toState.url !== 'login') {
-              AuthenticationService.pushPendingPath($location.url());
-            }
-            // Redirect to login
-            $location.path('/login');
-          } else if(toParams.organisation_id !== fromParams.organisation_id && !!toParams.organisation_id) {
-            var workspace = Session.getCurrentWorkspace();
-            if(workspace && ("o" + workspace.organisation_id + "d" + workspace.datamart_id === toParams.organisation_id || workspace.organisation_id === toParams.organisation_id)) {
-              $log.debug("done");
-            } else if(toState.name !== 'logout'){
-              $log.debug("prevent");
-              event.preventDefault();
-              Session.updateWorkspaceFromCurrentWorkspace(toParams.organisation_id).then(function(result) {
-                if(result) {
-                  $state.go(toState, toParams, {
-                    location: true, notify: true, reload: true
-                  });
-                } else {
-                  $state.go(toState, toParams, {
-                    location: true, notify: false, reload: false
-                  });
-                }
-              });
+          if (toState.publicUrl) {
+            $log.debug("nothing to check, public url !");
+          } else {
+            if (!AuthenticationService.hasRefreshToken()) {
+              if (toState.url !== 'login') {
+                AuthenticationService.pushPendingPath($location.url());
+              }
+              // Redirect to login
+              $location.path('/login');
+            } else if (toParams.organisation_id !== fromParams.organisation_id && !!toParams.organisation_id) {
+              var workspace = Session.getCurrentWorkspace();
+              if (workspace && ("o" + workspace.organisation_id + "d" + workspace.datamart_id === toParams.organisation_id || workspace.organisation_id === toParams.organisation_id)) {
+                $log.debug("done");
+              } else if (toState.name !== 'logout') {
+                $log.debug("prevent");
+                event.preventDefault();
+                Session.updateWorkspaceFromCurrentWorkspace(toParams.organisation_id).then(function (result) {
+                  if (result) {
+                    $state.go(toState, toParams, {
+                      location: true, notify: true, reload: true
+                    });
+                  } else {
+                    $state.go(toState, toParams, {
+                      location: true, notify: false, reload: false
+                    });
+                  }
+                });
+              }
             }
           }
         });
